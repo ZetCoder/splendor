519ad7de91c544d9112a3f33bd113a791d6e4cdb
Daniel Kwaśniewski <dakwasniewski@gmail.com>
2017-07-05 17:57:09 +0200

    first jsons handling

--- splendor-app/src/app/aristocrats/aristocrats.component.ts
+++ splendor-app/src/app/aristocrats/aristocrats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Aristocrats } from './aristocrats.model';
 
 @Component({
   selector: 'app-aristocrats',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aristocrats.component.css']
 })
 export class AristocratsComponent implements OnInit {
+  data: Object;
+  loading: boolean;
+  aristocrat: Aristocrats;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: Http) {
+    this.aristocrat = new Aristocrats(
+      0, ["0", "0", "0"], ""
+    );
   }
 
+  ngOnInit() {
+    this.makeRequest();
+  }
+
+  makeRequest(): void {
+    this.loading = true;
+    this.http.request('https://www.jasonbase.com/things/m4Ek')
+    .subscribe((res: Response) => {
+      this.data = res.json;
+      this.loading = false;
+      console.log(JSON.parse(res['_body']));
+    });
+  }
 }
--- splendor-app/src/app/cards/cards.component.ts
+++ splendor-app/src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  data: Object;
+  loading: boolean;
 
-  constructor() { }
+  //same as   constructor(http: Http) {this.http = http;}
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.makeRequest();
+  }
+
+  makeRequest(): void {
+    this.loading = true;
+    this.http.request('https://www.jasonbase.com/things/Q2b3')
+    .subscribe((res: Response) => {
+      this.data = res.json;
+      this.loading = false;
+      console.log(JSON.parse(res['_body']));
+    });
   }
 
 }
